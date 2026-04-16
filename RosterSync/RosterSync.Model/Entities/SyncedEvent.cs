97b8c031481028ded34c7330da18072130f2b96c
Manuel Schneider <57318033+schneidermanuel@users.noreply.github.com>
2026-04-16 08:00:39 +0200

    Update SyncedEvent.cs

--- RosterSync/RosterSync.Model/Entities/SyncedEvent.cs
+++ RosterSync/RosterSync.Model/Entities/SyncedEvent.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace RosterSync.Model.Entities;
 
